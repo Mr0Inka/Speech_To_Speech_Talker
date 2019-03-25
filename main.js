c8c78b19221879b09cf3a83ceb2d43cd67fab622
Thomas Weiß <thomas0weiss@googlemail.com>
2019-03-25 16:22:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-let process = {pro: false, spawn: false, file: false};
+let process = {pro: false, spawn: false, file: false, time: 0};
 
 const fs = require('fs');
 const Gpio = require('onoff').Gpio;
+const player = require('play-sound')(opts = {player: "mplayer"})
 
 const button = new Gpio(17, 'in', 'both', {debounceTimeout: 10});
 let recording = false;
+let audio;
+let lastRec;
+
+console.log("\n\n ! Press a button !\n\n")
 
 button.watch((err, value) => {
   if (err) {
@@ -15,15 +20,16 @@ button.watch((err, value) => {
   	stopRec();
   } else {
   	recording = true;
-  	startRec("recording.voc");
+  	startRec("button1.wav");
   }
 });
 
 function startRec(fileName){
-    console.log("Started: " + fileName)
+    console.log(" > Recording  " + fileName)
+    process.time = Date.now();
     process.file = fileName;
 	process.spawn = require('child_process').spawn;
-	process.pro = process.spawn('arecord', ['-D', 'sysdefault:CARD=1', '-f', 'dat', '-t', 'voc'].concat(fileName), {cwd: "./recordings"});
+	process.pro = process.spawn('arecord', ['-D', 'sysdefault:CARD=1', '-f', 'dat'].concat(fileName), {cwd: "./recordings"});
 	process.pro.on('exit', function(code, sig) {
 	    if (code !== null && sig === null) {
 	    }
@@ -34,11 +40,25 @@ function startRec(fileName){
 }
 
 function stopRec(){
+	lastRec = process.file;
     process.pro.kill('SIGTERM');
     process.pro = false;
-    process.spawn = false;
-    console.log("Stopped: " + process.file);
+    process.spawn = false;-
+    console.log("  - Recording finished! \n      - Length: " + ((Date.now() - process.time) / 1000).toFixed(1) + " second(s)\n      - Name: " + process.file + "\n");
     process.file = false;
+    process.time = 0;
+    playFile(lastRec)
 }
 
-
+function playFile(name){
+  console.log(" > Playing file " + name);
+  if(audio){
+    audio.kill()
+  }
+  audio = player.play("./recordings/" + name, function (err) {
+     if (err) {
+       console.log("Error: " + err);
+     }
+       console.log("  - File finished\n");
+  });
+}
